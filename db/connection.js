12a520bd0a67ebaed066aea6f42960bb53e7f062
Daniel Clarke <clarkit@gmail.com>
2018-07-13 21:47:01 +0100

    can now show total spent

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 
 module.exports = mysql.createPool({
   connectionLimit : 10,
+  multipleStatements: true,
   host: "localhost",
   user: "root",
   password: "root",
--- routes/user_products_routes.js
+++ routes/user_products_routes.js
@@ -8,20 +8,30 @@ const router = express.Router();
 
 router.get("/users/:id/products", (req, res) => {
 
-  const sql = `SELECT 
-    p.productName, 
-    p.productPrice
-    FROM product AS p
-    INNER JOIN transaction AS t 
-    ON p.productId = t.productId
-    INNER JOIN user AS u
-    ON t.userId = u.userId	
-    WHERE u.userId = ?;`
+  const productItemSql =
+    `SELECT 
+      p.productName, 
+      p.productPrice
+      FROM product AS p
+      INNER JOIN transaction AS t 
+      ON p.productId = t.productId
+      INNER JOIN user AS u
+      ON t.userId = u.userId	
+      WHERE u.userId = ?;
+    SELECT  
+      SUM(p.productPrice) as totalSpent
+      FROM product AS p
+      INNER JOIN transaction AS t 
+      ON p.productId = t.productId
+      INNER JOIN user AS u
+      ON t.userId = u.userId	
+      WHERE u.userId = ?;`
 
-  connectionPool.query(sql, [req.params.id], function (err, data) {
+  connectionPool.query(productItemSql, [req.params.id, req.params.id], function (err, data) {
     if(err) throw err;
     res.json(data);
   });
+
 });
 
 module.exports = router;
